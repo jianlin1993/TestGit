d8f2a55b2d6da77bb5bde86246413747707992b2
Rain <1015894275@qq.com>
2019-04-02 16:07:30 +0800

    初始化

---
+++ src/main/java/branch/ChildClass.java
@@ -0,0 +1,8 @@
+package branch;
+
+public class ChildClass extends SuperClass{
+    //master修改
+    static {
+        System.out.println("子类初始化！");
+    }
+}
---
+++ src/main/java/branch/SuperClass.java
@@ -0,0 +1,14 @@
+package branch;
+
+/**
+ * 被动使用类字段
+ * 通过子类引用父类的静态字段  不会导致子类的初始化
+ */
+public class SuperClass {
+    //master修改  不冲突
+    static{
+        System.out.println("父类初始化！");
+    }
+    public static int value=123;
+}
+
